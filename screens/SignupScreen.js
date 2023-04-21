dc84cf789322a96c703647408b44c28453960138
Your Name <urielparavi75@gmail.com>
2023-04-21 22:55:15 +0300

    177

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,7 +1,22 @@
+import { useState } from 'react';
 import AuthContent from '../components/Auth/AuthContent';
+import { createUser } from '../util/auth';
+import LoadingOverlay from '../components/ui/LoadingOverlay';
 
 function SignupScreen() {
-  return <AuthContent />;
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+
+  const signupHandler = async ({ email, password }) => {
+    setIsAuthenticating(true);
+    await createUser(email, password);
+    setIsAuthenticating(false);
+  };
+
+  if (isAuthenticating) {
+    return <LoadingOverlay message="Creating user..." />;
+  }
+
+  return <AuthContent onAuthenticate={signupHandler} />;
 }
 
 export default SignupScreen;
--- util/auth.js
+++ util/auth.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const API_KEY = 'AIzaSyCnbJ1uUffP-buVpxWN48by3h8ZuerE6I0';
 
-const createUser = async (email, password) => {
+export const createUser = async (email, password) => {
   const response = await axios.post(
     'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + API_KEY,
     {
