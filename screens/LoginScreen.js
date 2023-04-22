bfa006d1bad97d4289c399690d0243b35db22310
Your Name <urielparavi75@gmail.com>
2023-04-22 13:07:44 +0300

    178

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,7 +1,22 @@
+import { useState } from 'react';
 import AuthContent from '../components/Auth/AuthContent';
+import { login } from '../util/auth';
+import LoadingOverlay from '../components/ui/LoadingOverlay';
 
 function LoginScreen() {
-  return <AuthContent isLogin />;
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+
+  const loginHandler = async ({ email, password }) => {
+    setIsAuthenticating(true);
+    await login(email, password);
+    setIsAuthenticating(false);
+  };
+
+  if (isAuthenticating) {
+    return <LoadingOverlay message="Logging you in..." />;
+  }
+
+  return <AuthContent isLogin onAuthenticate={loginHandler} />;
 }
 
 export default LoginScreen;
--- util/auth.js
+++ util/auth.js
@@ -2,13 +2,21 @@ import axios from 'axios';
 
 const API_KEY = 'AIzaSyCnbJ1uUffP-buVpxWN48by3h8ZuerE6I0';
 
+const authenticate = async (mode, email, password) => {
+  const url =
+    `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=` + API_KEY;
+  const response = await axios.post(url, {
+    email: email,
+    password: password,
+    returnSecureToken: true,
+  });
+  console.log(response.data);
+};
+
 export const createUser = async (email, password) => {
-  const response = await axios.post(
-    'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + API_KEY,
-    {
-      email: email,
-      password: password,
-      returnSecureToken: true,
-    }
-  );
+  await authenticate('signUp', email, password);
+};
+
+export const login = async (email, password) => {
+  await authenticate('signInWithPassword', email, password);
 };
